2c43359af56d618a999767bb747d4429aae638fd
Rosalioalfre2 <rosalioalfre2@gmail.com>
2022-05-01 12:51:05 -0600

    Menu actividad

--- app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/ActividadMenuActivity.java
+++ app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/ActividadMenuActivity.java
@@ -1,14 +1,37 @@
 package com.edu.ues.fia.eisi.app_proyecto01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class ActividadMenuActivity extends AppCompatActivity {
+public class ActividadMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /*Varibles*/
+    public CardView DetalleActividad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_menu);
+
+        DetalleActividad = (CardView) findViewById(R.id.cardDetalleActividad);
+        DetalleActividad.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent i ;
+        switch (view.getId()) {
+            case R.id.cardDetalleActividad:
+                i = new Intent(this, MenuDetalleActividad.class);
+                //   i.putExtra("Rol",rol);
+                startActivity(i);
+                break;
+        }
+    }
+
+
 }
