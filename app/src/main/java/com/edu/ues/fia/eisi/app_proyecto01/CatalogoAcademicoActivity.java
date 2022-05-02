941c96c4453dc3a2751675640c16c64aa97e1121
Rosalioalfre2 <rosalioalfre2@gmail.com>
2022-05-01 23:10:38 -0600

    Configurado el catalogodo de actividades

--- app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/CatalogoAcademicoActivity.java
+++ app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/CatalogoAcademicoActivity.java
@@ -1,14 +1,63 @@
 package com.edu.ues.fia.eisi.app_proyecto01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class CatalogoAcademicoActivity extends AppCompatActivity {
+public class CatalogoAcademicoActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public CardView OfertaAcademica, Carrera, Escuela, Materia, DetalleOfertaAcademica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo_academico);
+
+        OfertaAcademica = (CardView) findViewById(R.id.cardOfertaAcademica);
+        OfertaAcademica.setOnClickListener(this);
+
+        Carrera = (CardView) findViewById(R.id.cardCarrera);
+        Carrera.setOnClickListener(this);
+
+        Escuela = (CardView) findViewById(R.id.cardEscuela);
+        Escuela.setOnClickListener(this);
+
+        Materia = (CardView) findViewById(R.id.cardMateria);
+        Materia.setOnClickListener(this);
+
+        DetalleOfertaAcademica = (CardView) findViewById(R.id.cardDetalleOferta);
+        DetalleOfertaAcademica.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i ;
+
+        switch (view.getId()){
+            case R.id.cardOfertaAcademica:
+                i = new Intent(this, MenuOfertaAcademica.class);
+                startActivity(i);
+                break;
+            case R.id.cardCarrera:
+                i = new Intent(this, MenuCarrera.class);
+                startActivity(i);
+                break;
+            case R.id.cardEscuela:
+                i = new Intent(this, MenuEscuela.class);
+                startActivity(i);
+                break;
+            case R.id.cardMateria:
+                i = new Intent(this, MenuMateria.class);
+                startActivity(i);
+                break;
+            case R.id.cardDetalleOferta:
+                i = new Intent(this, MenuDetalleOferta.class);
+                startActivity(i);
+                break;
+        }
     }
 }
