ce001226d99b4569da392a1cd3e719a0d5709089
Luis Molina <alfonsomduque@gmail.com>
2022-05-27 10:42:38 -0600

    Modificaciones visuaes para servicios web de Carrera

---
+++ app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/InsertarCarreraExterno.java
@@ -0,0 +1,19 @@
+package com.edu.ues.fia.eisi.app_proyecto01;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONObject;
+
+public class InsertarCarreraExterno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_carrera_externo);
+    }
+}
