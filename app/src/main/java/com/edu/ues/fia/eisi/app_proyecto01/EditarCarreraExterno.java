584cc67bf6fe1d10f1791f2256f28d24228f32d5
Luis Molina <alfonsomduque@gmail.com>
2022-05-28 01:13:37 -0600

    ELIMINAR REMOTO CARRERA con esto crud completo tabla carrera

--- app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/EditarCarreraExterno.java
+++ app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/EditarCarreraExterno.java
@@ -2,16 +2,18 @@ package com.edu.ues.fia.eisi.app_proyecto01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import org.json.JSONObject;
-
+@SuppressLint("NewApi")
 public class EditarCarreraExterno extends AppCompatActivity {
     EditText idCarrera, nombreCarrera;
     //Duque
     private final String urlLocal = "http://192.168.0.3/Proyecto1.2/ws_CARRERA_update.php";
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
