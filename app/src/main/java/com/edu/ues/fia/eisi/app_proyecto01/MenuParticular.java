b6b3c7ff6331b03aba50816ab18f2f3a9bb4ecae
Rosalio Monterrosa <rosalioalfre2@gmail.com>
2022-05-30 16:57:27 -0600

    Crud online particular

--- app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/MenuParticular.java
+++ app/src/main/java/com/edu/ues/fia/eisi/app_proyecto01/MenuParticular.java
@@ -8,18 +8,25 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MenuParticular extends AppCompatActivity implements View.OnClickListener {
-CardView cardInsertarParticular, cardEditarParticular, cardEliminarParticular, cardConsultarParticular;
+
+    public CardView insertar, consultar, editar, eliminar;
+    public CardView insertarOnline, consultarOnline, editarOnline, eliminarOnline;
+
     String tipoUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_particular);
 
-        //captura de datos
-        cardInsertarParticular = (CardView) findViewById(R.id.cardInsertarParticular);
-        cardEditarParticular = (CardView) findViewById(R.id.cardEditarParticular);
-        cardEliminarParticular = (CardView) findViewById(R.id.cardEliminarParticular);
-        cardConsultarParticular = (CardView) findViewById(R.id.cardConsultarParticular);
+        //Insertar
+        insertar = (CardView) findViewById(R.id.cardInsertar);
+        editar = (CardView) findViewById(R.id.cardEditar);
+        eliminar = (CardView) findViewById(R.id.cardEliminar);
+        consultar = (CardView) findViewById(R.id.cardConsultar);
+        insertarOnline = (CardView) findViewById(R.id.cardInsertarOnline);
+        editarOnline = (CardView) findViewById(R.id.cardEditarOnline);
+        eliminarOnline = (CardView) findViewById(R.id.cardEliminarOnline);
+        consultarOnline = (CardView) findViewById(R.id.cardConsultarOnline);
 
 
         Intent intent = getIntent();
@@ -28,18 +35,26 @@ CardView cardInsertarParticular, cardEditarParticular, cardEliminarParticular, c
             //   String opcioncRUD=tipoUsuario;
             switch (tipoUsuario) {
                 case "0100":
-                    cardInsertarParticular.setOnClickListener(this);
-                    cardEditarParticular.setOnClickListener(this);
-                    cardEliminarParticular.setOnClickListener(this);
-                    cardConsultarParticular.setOnClickListener(this);
+                    consultar.setOnClickListener(this);
+                    insertar.setOnClickListener(this);
+                    editar.setOnClickListener(this);
+                    eliminar.setOnClickListener(this);
+                    consultarOnline.setOnClickListener(this);
+                    insertarOnline.setOnClickListener(this);
+                    editarOnline.setOnClickListener(this);
+                    eliminarOnline.setOnClickListener(this);
                     break;
                 case "0200":
                 case "0400":
                 case "0600":
-                    cardInsertarParticular.setVisibility(View.GONE);
-                    cardEditarParticular.setVisibility(View.GONE);
-                    cardEliminarParticular.setVisibility(View.GONE);
-                    cardConsultarParticular.setOnClickListener(this);
+                    insertar.setVisibility(View.GONE);
+                    editar.setVisibility(View.GONE);
+                    eliminar.setVisibility(View.GONE);
+                    consultar.setOnClickListener(this);
+                    insertarOnline.setVisibility(View.GONE);
+                    editarOnline.setVisibility(View.GONE);
+                    eliminarOnline.setVisibility(View.GONE);
+                    consultarOnline.setOnClickListener(this);
                     break;
                 case "0500":
                     break;
@@ -47,10 +62,14 @@ CardView cardInsertarParticular, cardEditarParticular, cardEliminarParticular, c
             }
         }
 
-        cardInsertarParticular.setOnClickListener(this);
-        cardEditarParticular.setOnClickListener(this);
-        cardEliminarParticular.setOnClickListener(this);
-        cardConsultarParticular.setOnClickListener(this);
+        insertar.setOnClickListener(this);
+        editar.setOnClickListener(this);
+        eliminar.setOnClickListener(this);
+        consultar.setOnClickListener(this);
+        insertarOnline.setOnClickListener(this);
+        editarOnline.setOnClickListener(this);
+        eliminarOnline.setOnClickListener(this);
+        consultarOnline.setOnClickListener(this);
 
 
     }
@@ -59,28 +78,38 @@ CardView cardInsertarParticular, cardEditarParticular, cardEliminarParticular, c
     public void onClick(View view) {
         Intent i ;
         switch (view.getId()){
-            case R.id.cardInsertarParticular:
-                i=new Intent(this, InsertarParticularActivity.class);
-                //   i.putExtra("Rol",rol);
-                 startActivity(i);
-                break;
-            case R.id.cardEditarParticular:
-                i=new Intent(this, EditarParticularActivity.class);
-                //   i.putExtra("Rol",rol);
+            case R.id.cardInsertar:
+                i = new Intent(this, InsertarParticularActivity.class);
                 startActivity(i);
                 break;
-            case R.id.cardConsultarParticular:
-                i=new Intent(this, ConsultarParticularActivity.class);
-                //   i.putExtra("Rol",rol);
+            case R.id.cardConsultar:
+                i = new Intent(this, ConsultarParticularActivity.class);
                 startActivity(i);
                 break;
-            case R.id.cardEliminarParticular:
-                i=new Intent(this, EliminarParticularActivity.class);
-                //   i.putExtra("Rol",rol);
+            case R.id.cardEditar:
+                i = new Intent(this, EditarParticularActivity.class);
                 startActivity(i);
                 break;
-
-
+            case R.id.cardEliminar:
+                i = new Intent(this, EliminarParticularActivity.class);
+                startActivity(i);
+                break;
+            case R.id.cardInsertarOnline:
+                i = new Intent(this, InsertarParticularExterno.class);
+                startActivity(i);
+                break;
+            case R.id.cardEliminarOnline:
+                i = new Intent(this, EliminarParticularExterno.class);
+                startActivity(i);
+                break;
+            case R.id.cardConsultarOnline:
+                i = new Intent(this, ConsultarParticularExterno.class);
+                startActivity(i);
+                break;
+            case R.id.cardEditarOnline:
+                i = new Intent(this, EditarParticularExterno.class);
+                startActivity(i);
+                break;
         }
     }
 }
